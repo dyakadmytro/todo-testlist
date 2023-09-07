9f75f3b937ba84f33027e71609c9a50a98e2415f
Dmitriy <dmitryneposidjaka@gmail.com>
2023-09-07 16:29:05 +0300

    add login, logout

---
+++ database/migrations/2023_09_07_130657_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\User::class);
+            $table->foreignIdFor(\App\Models\Tasklist::class);
+            $table->string('name', 32);
+            $table->tinyText('description')->nullable();
+            $table->enum('status', ['todo', 'done']);
+            $table->smallInteger('priority')->unsigned()->default(1);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('completed_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
