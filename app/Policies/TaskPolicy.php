7468f4dc9b520d6b3e0e312a90559c24316d5687
Dmitriy <dmitryneposidjaka@gmail.com>
2023-09-08 17:11:22 +0300

    add TaskList and Task policy

---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Task;
+use App\Models\TaskList;
+use App\Models\User;
+
+class TaskPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Task $task): bool
+    {
+        return $task->user->id === $user->id;
+    }
+
+    public function createTask(User $user, TaskList $taskList): bool
+    {
+        return $taskList->user->id === $user->id;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Task $task): bool
+    {
+        return $task->user->id === $user->id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Task $task): bool
+    {
+        return $task->user->id === $user->id;
+    }
+}
